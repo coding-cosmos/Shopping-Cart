4a9065fccf9ca18d4cf8df3333e74930edd288a9
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-06-14 03:25:36 -0700

    Update App.jsx to use routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
+import routes from "./routes";
+
+const router = createBrowserRouter(routes);
 
 function App() {
-  return <></>;
+  return <><RouterProvider router={router}/></>;
 }
 
 export default App;
