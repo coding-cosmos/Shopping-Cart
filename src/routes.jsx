8475cabbd5db258637b098d6d1844bef665a653e
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-06-14 06:25:06 -0700

    Add routes for <ShopPage/>

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,16 @@
 import HomePage from "./pages/HomePage/HomePage";
 import ErrorPage from "./pages/ErrorPage/ErrorPage";
+import ShopPage from "./pages/ShopPage/ShopPage";
 
 const routes = [
   {
     path: "/",
     element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "shop",
+    element: <ShopPage />,
     errorElement: <ErrorPage />,
   }
 ];
