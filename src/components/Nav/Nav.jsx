d0fb4e8a5a4bb816a7d4ce542483b01600449d81
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-06-14 08:52:52 -0700

    Update Nav to change active states as per URL location

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,19 +1,22 @@
+import { useLocation } from "react-router-dom";
 import CartButton from "../CartButton/CartButton";
 import PageLink from "../PageLink/PageLink";
 import styles from "./Nav.module.css";
 import PropType from 'prop-types';
 
-const Nav = ({page}) => {
+const Nav = () => {
+  const location = useLocation();
+  const page = location.pathname;
   return (
     <>
       <div className={styles.nav}>
         <div className={styles.logo}>CART</div>
         <div className={styles.links}>
-          <PageLink route="/" name="Home" active={page == 'home'} />
-          <PageLink route="/shop" name="Shop" active={page == 'shop'} />
+          <PageLink route="/" name="Home" active={page == '/' || page=='/cart'} />
+          <PageLink route="/shop" name="Shop" active={page == '/shop' || page=="/shop/cart"} />
         </div>
 
-        <CartButton active={false} />
+        <CartButton active={page=="/cart" || page=="/shop/cart"} />
       </div>
     </>
   );
