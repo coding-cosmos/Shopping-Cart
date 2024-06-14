9418ae41ed6127e6fbebbfceabb5f422acf26316
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-06-14 10:13:36 -0700

    Add functionality to open and close the cart page by clicking the button

--- src/components/CartButton/CartButton.jsx
+++ src/components/CartButton/CartButton.jsx
@@ -1,12 +1,22 @@
 import styles from "./CartButton.module.css";
 import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
+import { useLocation,useNavigate } from "react-router-dom";
 
 const CartButton = ({active})=>{
     const activeClass = active?styles.cart_active:styles.cart;
     const icon = active?"/images/cart-outline.svg":"/images/cart.svg";
+    const navigate = useNavigate();
+    const location = useLocation();
+    const path = location.pathname;
 
-    return <Link to="cart">
+    const handleClick = ()=>{
+        if(path=="/cart"||path=="/shop/cart"){
+            navigate(-1);
+        }
+    };
+
+    return <Link to="cart" onClick={handleClick}>
         <div className={activeClass}>
             <img className={styles.img} src={icon} alt="" />
         </div>
