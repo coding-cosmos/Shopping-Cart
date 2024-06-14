d43a4b81715fdfe0da59cf0411906a5f0d933d70
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-06-14 06:23:20 -0700

    Update Nav to use page prop

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,20 +1,27 @@
-import CartButton from '../CartButton/CartButton';
-import PageLink from '../PageLink/PageLink';
-import styles from './Nav.module.css';
+import CartButton from "../CartButton/CartButton";
+import PageLink from "../PageLink/PageLink";
+import styles from "./Nav.module.css";
+import PropType from 'prop-types';
 
-const Nav = ()=>{
-    return <>
-    <div className={styles.nav}>
+const Nav = ({page}) => {
+  return (
+    <>
+      <div className={styles.nav}>
         <div className={styles.logo}>CART</div>
         <div className={styles.links}>
-            <PageLink route="/" name="Home" active='true'/>
-            <PageLink route="shop" name="Shop"/>
+          <PageLink route="/" name="Home" active={page == 'home'} />
+          <PageLink route="/shop" name="Shop" active={page == 'shop'} />
         </div>
-       
-            <CartButton active={false}/>
 
-    </div>
+        <CartButton active={false} />
+      </div>
     </>
+  );
 };
+
+Nav.propTypes = {
+    page: PropType.string.isRequired,
+}
+
 
 export default Nav;
