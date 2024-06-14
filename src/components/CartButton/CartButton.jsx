465d427059201ef7b75a4274f7ae9fde5d07babb
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-06-14 08:34:38 -0700

    Update icon path

--- src/components/CartButton/CartButton.jsx
+++ src/components/CartButton/CartButton.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const CartButton = ({active})=>{
     const activeClass = active?styles.cart_active:styles.cart;
-    const icon = active?"cart-outline.svg":"cart.svg";
+    const icon = active?"/images/cart-outline.svg":"/images/cart.svg";
 
     return <Link to="cart">
         <div className={activeClass}>
