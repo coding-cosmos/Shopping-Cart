06103ad5a7ad71d20cc8dce20746be46f4c75b19
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-06-14 12:51:48 -0700

    Add hero sectin and products section

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,31 @@
 import Nav from "../../components/Nav/Nav";
 import { Outlet } from "react-router-dom";
+import styles from "./HomePage.module.css";
+import HeroCarousel from "../../components/HeroCarousel/HeroCarousel";
+import ProductCarousel from "../../components/ProductCarousel/ProductCarousel";
+import PageLink from "../../components/PageLink/PageLink";
 
-
-const Home = ()=>{
-    return <>
-    <Nav/>
-    <Outlet/>
-    </>
+const Home = () => {
+  return (
+    <div className="home">
+      <Nav />
+      <div className={styles.main__wrapper}>
+        <div className={styles.main__content}>
+            <section className={styles.hero}>
+                <div className={styles.hero__content}>
+                    <h1 className={styles.heading}>Shop the World!!</h1>
+                    <PageLink route="shop" name="Shop Now" active={true} className={styles.cta}/>
+                </div>
+                <HeroCarousel/>
+            </section>
+            <section className={styles.products}>
+                <ProductCarousel/>
+            </section>
+        </div>
+        <Outlet />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
