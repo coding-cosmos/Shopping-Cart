7ec74d82695c3ae829c808cb986f130b9c1445c3
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-06-14 08:54:05 -0700

    Add Cart as children

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,18 +1,31 @@
 import HomePage from "./pages/HomePage/HomePage";
 import ErrorPage from "./pages/ErrorPage/ErrorPage";
 import ShopPage from "./pages/ShopPage/ShopPage";
+import Cart from "./components/Cart/Cart";
 
 const routes = [
   {
     path: "/",
     element: <HomePage />,
     errorElement: <ErrorPage />,
+    children:[
+      {
+        path:"cart",
+        element:<Cart/>
+      }
+    ]
   },
   {
     path: "shop",
     element: <ShopPage />,
     errorElement: <ErrorPage />,
-  }
+    children:[
+      {
+        path:"cart",
+        element:<Cart/>
+      }
+    ]
+  },
 ];
 
 export default routes;
