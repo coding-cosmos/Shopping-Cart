81bc3785b786f709ba94d7b23acf38a3695c4dd7
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-06-15 21:57:55 +0530

    Update page to use ProductSection component

--- src/pages/ShopPage/ShopPage.jsx
+++ src/pages/ShopPage/ShopPage.jsx
@@ -1,11 +1,25 @@
 import { Outlet } from "react-router-dom";
 import Nav from "../../components/Nav/Nav";
+import styles from "./ShopPage.module.css";
+import ProductSection from "../../components/ProductSection/ProductSection";
 
-
-const ShopPage = ()=>{
-    return <><Nav/>
-    <Outlet/>
+const ShopPage = () => {
+  return (
+    <>
+      <Nav />
+      <div className={styles.main__wrapper}>
+        <div className={styles.main__content}>
+          <section className={styles.products}>
+            <h1 className={styles.heading}>Explore</h1>
+            <div className={styles.cards_wrapper}>
+              <ProductSection />
+            </div>
+          </section>
+        </div>
+        <Outlet />
+      </div>
     </>
+  );
 };
 
 export default ShopPage;
