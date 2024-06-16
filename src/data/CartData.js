8624784d20a0e2808dea773ded5ec4f72ea07fa4
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-06-16 19:59:04 +0530

    Add different functions to update and access cart data

--- src/data/CartData.js
+++ src/data/CartData.js
@@ -1,12 +1,48 @@
-function addItem(item){
-    const items = JSON.parse(localStorage.getItem('cart'));
-    const updatedItems = items?[...items,item]:[item];
-    localStorage.setItem('cart',JSON.stringify(updatedItems));
-    window.dispatchEvent(new Event('storage'));
+function updateCart(items) {
+  localStorage.setItem("cart", JSON.stringify(items));
+  window.dispatchEvent(new Event("storage"));
 }
 
-function getNoOfItems(){
-    const items = JSON.parse(localStorage.getItem('cart'));
-    return items?items.length:0;
+function addItem(item) {
+  const items = JSON.parse(localStorage.getItem("cart"));
+  const updatedItems = items ? [...items, item] : [item];
+  updateCart(updatedItems);
 }
-export  {addItem,getNoOfItems};
+
+function getNoOfItems() {
+  const items = JSON.parse(localStorage.getItem("cart"));
+  return items ? items.length : 0;
+}
+
+function getItems() {
+  const items = JSON.parse(localStorage.getItem("cart"));
+  return items ? items : null;
+}
+
+function removeItem(item) {
+  const items = JSON.parse(localStorage.getItem("cart"));
+  const index = items.findIndex((itemItr) => itemItr.title === item.title);
+  items.splice(index, 1);
+  updateCart(items);
+}
+
+function updateQuantity(item, quantity) {
+  const items = JSON.parse(localStorage.getItem("cart"));
+  const index = items.findIndex((itemItr) => itemItr.title === item.title);
+  items[index].quantity = quantity;
+  updateCart(items);
+}
+
+function getTotalCost() {
+  const items = JSON.parse(localStorage.getItem("cart"));
+  return items.reduce((sum, item) => sum + item.price * item.quantity, 0);
+}
+
+export {
+  addItem,
+  getNoOfItems,
+  getItems,
+  removeItem,
+  updateQuantity,
+  getTotalCost,
+};
