0b203a865514091d964a403084ea426432802740
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-06-16 16:59:16 +0530

    Update to show number of items in the cart

--- src/components/CartButton/CartButton.jsx
+++ src/components/CartButton/CartButton.jsx
@@ -1,29 +1,47 @@
 import styles from "./CartButton.module.css";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import { useLocation,useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { getNoOfItems } from "../../data/CartData";
+import { useEffect, useState } from "react";
 
-const CartButton = ({active})=>{
-    const activeClass = active?styles.cart_active:styles.cart;
-    const icon = active?"/images/cart-outline.svg":"/images/cart.svg";
-    const navigate = useNavigate();
-    const location = useLocation();
-    const path = location.pathname;
+const CartButton = ({ active }) => {
+  const activeClass = active ? styles.cart_active : styles.cart;
+  const quantityClass = active ? styles.quantity_active : styles.quantity;
+  const icon = active ? "/images/cart-outline.svg" : "/images/cart.svg";
 
-    const handleClick = ()=>{
-        if(path=="/cart"||path=="/shop/cart"){
-            navigate(-1);
-        }
+  const navigate = useNavigate();
+  const location = useLocation();
+  const path = location.pathname;
+  const [quantity, setQuantity] = useState(getNoOfItems());
+
+  const handleClick = () => {
+    if (path == "/cart" || path == "/shop/cart") {
+      navigate(-1);
+    }
+  };
+
+  useEffect(() => {
+    const handleStorage = () => {
+      setQuantity(getNoOfItems());
     };
 
-    return <Link to="cart" onClick={handleClick}>
-        <div className={activeClass}>
-            <img className={styles.img} src={icon} alt="" />
-        </div>
+    window.addEventListener("storage", handleStorage);
+
+    return () => window.removeEventListener("storage", handleStorage);
+  }, []);
+
+  return (
+    <Link to="cart" onClick={handleClick} className={styles.link}>
+      <div className={activeClass}>
+        <img className={styles.img} src={icon} alt="" />
+        <div className={quantityClass}>{quantity}</div>
+      </div>
     </Link>
-}
+  );
+};
 
 CartButton.propTypes = {
-    active: PropTypes.bool,
-}
+  active: PropTypes.bool,
+};
 export default CartButton;
